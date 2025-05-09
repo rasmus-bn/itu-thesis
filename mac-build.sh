275899c80b0efcfa08fa8d896d9f03bacdebb787
Jan Lishak <janlishak@users.noreply.github.com>
2025-05-09 12:51:15 +0200

    mac build process

---
+++ mac-build.sh
@@ -0,0 +1,17 @@
+echo "[INFO] Building Docker image..."
+
+docker buildx build --platform linux/amd64 -t robots:latest \
+  --output type=docker,dest=robots.tar .
+
+echo "[INFO] Converting Docker image to SIF..."
+
+docker run --rm \
+  --privileged \
+  -v "$(pwd)":/workspace \
+  -w /workspace \
+  ghcr.io/apptainer/apptainer \
+  apptainer build --arch amd64 --force robots.sif oci-archive://robots.tar
+
+echo "[INFO] Copying to cluster"
+scp ./robots.sif jlis@hpc.itu.dk:/home/jlis/
+echo "[INFO] Done."
