7b4c7558ff485bc7cf9b8faa42fe759c80784604
Jan Lishak <janlishak@users.noreply.github.com>
2025-05-19 01:36:42 +0200

    plot for fitness function

---
+++ tests/test_23.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# Define collected resources and completed times
+collected_resources = list(range(0, 6)) + [5]*6
+completed_time = [60]*6 + list(range(50, 0, -10))
+
+# Calculate fitness values
+fitness = [60 / t * c for c, t in zip(collected_resources, completed_time)]
+
+# Print in format: collected, time, fitness
+for c, t, f in zip(collected_resources, completed_time, fitness):
+    print(f"{c}, {t}, {f:.2f}")
+
+# Plot
+plt.figure(figsize=(10, 6))
+plt.plot(range(len(fitness)), fitness, marker='o')
+plt.axvline(x=5, color='red', linestyle='--')  # Vertical red line at index 5
+plt.title("Fitness Function")
+plt.xlabel("Solution Index")
+plt.ylabel("Fitness")
+plt.grid(True)
+plt.show()
